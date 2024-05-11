ff0c1367b0fba7180496f64515c6f083c800ed55
Satya <satya05112003@gmail.com>
2024-05-12 00:42:32 +0530

    added cart total

--- components/Cart/cart.tsx
+++ components/Cart/cart.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Minus, Plus, Trash, Trash2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import {Drawer,DrawerContent,DrawerFooter,DrawerHeader,DrawerTitle,DrawerClose,} from "@/components/ui/drawer";
+import { Drawer, DrawerContent, DrawerFooter, DrawerHeader, DrawerTitle, DrawerClose, } from "@/components/ui/drawer";
 import { useCart } from "@/components/Cart/cartContext";
 import { on } from "events";
 
@@ -10,8 +10,8 @@ interface CartDrawerProps {
   onOpen: boolean;
 }
 
-export function CartDrawer({ onClose ,onOpen}: CartDrawerProps) {
-  const { cart, incrementQuantity, decrementQuantity, removeFromCart } =
+export function CartDrawer({ onClose, onOpen }: CartDrawerProps) {
+  const { cart, incrementQuantity, decrementQuantity, removeFromCart,totalCost } =
     useCart();
 
   return (
@@ -25,24 +25,20 @@ export function CartDrawer({ onClose ,onOpen}: CartDrawerProps) {
             <div key={item.id} className="flex items-center justify-between mb-4">
               <div>
                 <div>{item.name}</div>
-                <div>${item.price.toFixed(2)}</div>
+                <div>₹{item.price.toFixed(2)}</div>
               </div>
               <div className="flex items-center gap-6 text-2xl">
-                {/* <Button onClick={() => decrementQuantity(item.id)}> */}
-                  {/* <Minus className="h-4 w-4" /> */}
-                  <Minus onClick={() => decrementQuantity(item.id)} className="h-4 w-4 cursor-pointer" />
-
-                {/* </Button> */}
+                <Minus onClick={() => decrementQuantity(item.id)} className="h-4 w-4 cursor-pointer" />
                 <div>{item.quantity}</div>
-                {/* <Button onClick={() => incrementQuantity(item.id)}> */}
-                  <Plus onClick={() => incrementQuantity(item.id)} className="h-4 w-4 cursor-pointer" />
-                {/* </Button> */}
-                {/* <Trash2 onClick={()=> removeFromCart(item.id)} className="fill-red-600"></Trash2> */}
-                <Button onClick={()=> removeFromCart(item.id)} variant="destructive">Remove</Button>
-                
+                <Plus onClick={() => incrementQuantity(item.id)} className="h-4 w-4 cursor-pointer" />
+                <Button onClick={() => removeFromCart(item.id)} variant="destructive">Remove</Button>
               </div>
             </div>
           ))}
+          <div className="flex justify-between mt-4 font-bold">
+            <div>Total:</div>
+            <div>₹{totalCost.toFixed(2)}</div>
+          </div>
         </div>
         <DrawerFooter>
           <DrawerClose asChild>
